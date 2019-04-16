44a59c4e762b715d4b91547bccfdd21fa14edbe8
bms-jx <afetene@cisco.com>
2019-04-16 11:07:44 -0400

    init commit

---
+++ src/main/java/com/bms/jx/test/springappdemo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.bms.jx.test.springappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
